f1eea1ccd0c372abc3f8a185ff62524818357006
saf <1197931933@qq.com>
2019-04-14 15:22:13 +0800

    01

---
+++ text01/src/main/java/com/example/text01/Text01Application.java
@@ -0,0 +1,13 @@
+package com.example.text01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Text01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Text01Application.class, args);
+	}
+
+}
